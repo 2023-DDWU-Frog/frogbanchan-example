210a0d6f0f14681fc303d09c756aeffa176cc995
chooorock <reiche7777@naver.com>
2023-05-11 14:16:24 +0900

    [feat] placeDAO&JDBC test
    동일

---
+++ src/main/java/com/example/frogbanchan/dao/jdbc/PlaceDaoJDBC.java
@@ -0,0 +1,58 @@
+package com.example.frogbanchan.dao.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.example.frogbanchan.domain.Place;
+
+public class PlaceDaoJDBC {
+
+    private JDBCUtil jdbcUtil = null; // JDBCUtil 객체를 참조하기 위한 변수
+
+    public PlaceDaoJDBC() { // 생성자
+        jdbcUtil = new JDBCUtil(); // JDBCUtil 객체 생성
+    }
+
+    public Place findPlaceById(String pid) throws SQLException {
+        String sql = "SELECT * FROM PLACE WHERE PLACE_ID = ? ";
+        jdbcUtil.setSqlAndParameters(sql, new Object[] { pid }); // JDBCUtil에 query문과 매개 변수 설정
+
+        Place place = new Place();
+
+        try {
+            ResultSet rs = jdbcUtil.executeQuery(); // query 실행
+            if (rs.next()) {
+                String placeId = rs.getString("PLACE_ID");
+                String name = rs.getString("NAME");
+                String address = rs.getString("ADDRESS");
+                place = new Place(placeId, name, address);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.close(); // resource 반환
+        }
+        return place;
+    }
+
+    public void insertPlace(Place place) throws SQLException {
+        String sql = "INSERT INTO PLACE VALUES";
+        jdbcUtil.setSqlAndParameters(sql, new Object[] { place.getPlaceId(), p }); // JDBCUtil에 query문과 매개 변수 설정
+
+        try {
+            ResultSet rs = jdbcUtil.executeQuery(); // query 실행
+            if (rs.next()) {
+                String placeId = rs.getString("PLACE_ID");
+                String name = rs.getString("NAME");
+                String address = rs.getString("ADDRESS");
+                place = new Place(placeId, name, address);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.close(); // resource 반환
+        }
+
+    }
+
+}
--- src/main/java/com/example/frogbanchan/domain/Place.java
+++ src/main/java/com/example/frogbanchan/domain/Place.java
@@ -1,5 +1,101 @@
 package com.example.frogbanchan.domain;
 
+import java.security.Timestamp;
+import java.util.List;
+
 public class Place {
+    private String placeId;
+    private String password;
+    private String name;
+    private String address;
+    private double totalScore;
+    private List<PlaceMenu> menuList;
+    private List<Timestamp> availableTime;
+    private List<Reservation> reservationList;
+
+    public Place() {
+    }
+
+    public Place(String placeId, String name, String address) {
+        this.placeId = placeId;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Place(String placeId, String password, String name, String address, double totalScore,
+            List<PlaceMenu> menuList, List<Timestamp> availableTime, List<Reservation> reservationList) {
+        this.placeId = placeId;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.totalScore = totalScore;
+        this.menuList = menuList;
+        this.availableTime = availableTime;
+        this.reservationList = reservationList;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(double totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public List<PlaceMenu> getMenuList() {
+        return menuList;
+    }
+
+    public void setMenuList(List<PlaceMenu> menuList) {
+        this.menuList = menuList;
+    }
+
+    public List<Timestamp> getAvailableTime() {
+        return availableTime;
+    }
+
+    public void setAvailableTime(List<Timestamp> availableTime) {
+        this.availableTime = availableTime;
+    }
+
+    public List<Reservation> getReservationList() {
+        return reservationList;
+    }
+
+    public void setReservationList(List<Reservation> reservationList) {
+        this.reservationList = reservationList;
+    }
 
 }
