3669ed3cd58283c66bd275877668b947d844a057
ddyearn <bestsy777@gmail.com>
2023-05-11 10:02:40 +0900

    Feat: update domain

--- src/main/java/com/example/frogbanchan/domain/History.java
+++ src/main/java/com/example/frogbanchan/domain/History.java
@@ -1,5 +1,48 @@
 package com.example.frogbanchan.domain;
 
-public class History {
+import java.io.Serializable;
+import java.sql.Timestamp;
 
+@SuppressWarnings("serial")
+public class History implements Serializable {
+
+    /* Private Fields */
+    private String username;
+    Timestamp recorded_date;
+    private int menu_id;
+    private int place_id;
+    private int score;
+
+    /* JavaBeans Properties */
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Timestamp getRecorded_date() {
+        return recorded_date;
+    }
+
+    public int getMenu_id() {
+        return menu_id;
+    }
+    public void setMenu_id(int menu_id) {
+        this.menu_id = menu_id;
+    }
+
+    public int getPlace_id() {
+        return place_id;
+    }
+    public void setPlace_id(int place_id) {
+        this.place_id = place_id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
